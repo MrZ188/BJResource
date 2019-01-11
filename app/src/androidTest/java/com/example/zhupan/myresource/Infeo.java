25ffaf290c152e9719a4667bdef7ec9a5b1abca5
zhupan <zhupan@bojoy.com>
2019-01-11 18:47:08 +0800

    change

--- app/src/androidTest/java/com/example/zhupan/myresource/Infeo.java
+++ app/src/androidTest/java/com/example/zhupan/myresource/Infeo.java
@@ -7,4 +7,5 @@ package com.example.zhupan.myresource;
 public class Infeo {
 //    https://go2think.com/bt-magnet-search/
 //    https://www.jianshu.com/p/796b3e02c319
+//    https://movie.douban.com/typerank?type_name=%E7%A7%91%E5%B9%BB&type=17&interval_id=100:90&action=
 }
